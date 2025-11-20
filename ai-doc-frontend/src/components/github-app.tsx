762022b1620a724addbe21a40f9094a3805ce003
Johannes Flodin <jannis020609@gmail.com>
2025-11-20 11:01:58 +0100

    formatted

--- ai-doc-frontend/src/components/github-app.tsx
+++ ai-doc-frontend/src/components/github-app.tsx
@@ -6,12 +6,12 @@ export default function InstallButton() {
   const redirectUrl = `https://github.com/apps/${process.env.NEXT_PUBLIC_GITHUB_APP_NAME}/installations/new`;
 
   return (
-            <button
-              className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2 flex flex-row items-center gap-2 hover:scale-105 transition-transform"
-              onClick={() => window.location.assign(redirectUrl)}
-            >
-              <IconBrandGithub />
-              Connect your repositories
-            </button>
+    <button
+      className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2 flex flex-row items-center gap-2 hover:scale-105 transition-transform"
+      onClick={() => window.location.assign(redirectUrl)}
+    >
+      <IconBrandGithub />
+      Connect your repositories
+    </button>
   );
 }
